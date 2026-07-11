5ef5d8116ab5efe8e08c026f2aac49a9aa11efa9
Daniel-png06 <dabuh@byupathway.edu>
2026-07-11 10:20:41 +0100

    Fix cart empty error

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,41 +3,55 @@ import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
 
+  const productList = document.querySelector(".product-list");
+
+  // Display a message if the cart is empty
+  if (cartItems.length === 0) {
+    productList.innerHTML = `
+      <li class="cart-card divider">
+        <p>Your cart is empty.</p>
+      </li>
+    `;
+    return;
+  }
+
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
 
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  productList.innerHTML = htmlItems.join("");
 
   attachQuantityListeners();
 }
 
 function cartItemTemplate(item) {
-  return `<li class="cart-card divider">
-    <a href="#" class="cart-card__image">
-      <img
-        src="${item.Image}"
-        alt="${item.Name}"
-      />
-    </a>
+  return `
+    <li class="cart-card divider">
+      <a href="#" class="cart-card__image">
+        <img
+          src="${item.Image}"
+          alt="${item.Name}"
+        />
+      </a>
 
-    <a href="#">
-      <h2 class="card__name">${item.Name}</h2>
-    </a>
+      <a href="#">
+        <h2 class="card__name">${item.Name}</h2>
+      </a>
 
-    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+      <p class="cart-card__color">${item.Colors[0].ColorName}</p>
 
-    <p class="cart-card__quantity">
-      Qty:
-      <input
-        class="cart-qty"
-        type="number"
-        min="1"
-        value="${item.quantity || 1}"
-        data-id="${item.Id}"
-      >
-    </p>
+      <p class="cart-card__quantity">
+        Qty:
+        <input
+          class="cart-qty"
+          type="number"
+          min="1"
+          value="${item.quantity || 1}"
+          data-id="${item.Id}"
+        >
+      </p>
 
-    <p class="cart-card__price">$${item.FinalPrice}</p>
-  </li>`;
+      <p class="cart-card__price">$${item.FinalPrice}</p>
+    </li>
+  `;
 }
 
 function attachQuantityListeners() {
